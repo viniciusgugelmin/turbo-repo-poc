9269f5f1d413d46b91e68acc3b83ef26708356b9
Vinicius <vinigugelmin@gmail.com>
2023-08-29 17:17:51 -0300

    feat: state added to packaged

--- turborepo/apps/web/app/page.tsx
+++ turborepo/apps/web/app/page.tsx
@@ -1,10 +1,22 @@
-import { Button, Header } from "ui";
+"use client"
+import { Button, Header, GlobalContextProvider, useGlobalContext } from "ui";
 
 export default function Page(): JSX.Element {
   return (
     <>
+    <GlobalContextProvider>
       <Header text="Web" />
       <Button />
+      <LoremComponent />
+    </GlobalContextProvider>
     </>
   );
+}
+
+function LoremComponent() {
+  const { value, setValue } = useGlobalContext()
+
+  console.log(value)
+
+  return <div><button onClick={() => setValue(value === "default" ? "other" : "default")}>{value}</button></div>
 }
